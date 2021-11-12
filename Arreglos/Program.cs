497a43bb3be2a87b50eea0ba873f0dca3ffcd70e
Hang <han.galvis@uniagustiniana.edu.co>
2021-11-12 08:28:16 -0500

    third commit

---
+++ Arreglos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Condicionales/Program.cs
+++ Condicionales/Program.cs
@@ -6,7 +6,34 @@ namespace Condicionales
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int edad = 18;
+            if (edad >= 18)
+            {
+                Console.WriteLine("puedes ingresar");
+            }
+            else
+            {
+                Console.WriteLine("no puedes ingresar");
+            }
+
+            char nota = 'C';
+
+            switch (nota)
+            {
+                case 'A':
+                    Console.WriteLine("Nota superior");
+                    break;
+                case 'B':
+                    Console.WriteLine("Nota Intermedia");
+                    break;
+                case 'C':
+                    Console.WriteLine("nota baja");
+                    break;
+                default:
+                    Console.WriteLine("Dato ingresado no valido");
+                    break;
+            }
+
         }
     }
 }
